faacb910d9f4c741f5b83311f04fed148928ed94
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-07-11 09:03:22 -0400

    removed "set" function

--- test/index.js
+++ test/index.js
@@ -2,11 +2,9 @@ import "source-map-support/register";
 import tinyTest from "tiny-test";
 import validatorTest from "./validator";
 import getTypeInterfaceTest from "./get-type-interface";
-import setTest from "./set";
 
 tinyTest((test, load) => {
   validatorTest(test);
   getTypeInterfaceTest(test);
-  setTest(test);
   load();
 });
